f219548cd58b505e0473075798ab88c1291fc666
RafalGrabowski95 <rafal.grabowski@naturalantybody.com>
2022-12-30 18:16:39 +0100

    elo

--- lint-staged.js
+++ lint-staged.js
@@ -1,4 +1,8 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --max-warnings=0', () => 'tsc-files --noEmit'],
+  '*.{js,jsx,ts,tsx}': [
+    'eslint --max-warnings=0',
+    // 'react-scripts test --bail --findRelatedTests --passWithNoTests',
+    () => 'tsc-files --noEmit',
+  ],
   '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
 };
