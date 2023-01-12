9727ed4dec954e6e237d244602ad2a6de35a2a34
RafalGrabowski95 <rafal.grabowski@naturalantybody.com>
2023-01-12 23:12:40 +0100

    feat(input): added input atoms

--- components/screens/Settings/Settings.tsx
+++ components/screens/Settings/Settings.tsx
@@ -1,9 +1,13 @@
 import { Text } from 'components/atoms/Text';
+import { Input } from 'components/atoms/Input';
 
 export const Settings = () => {
   return (
-    <Text primary styles="bg-white dark:bg-red-500">
-      Settings
-    </Text>
+    <>
+      <Text primary styles="bg-white dark:bg-red-500">
+        Settings
+      </Text>
+      <Input placeholder="elo" suffix="kg" />
+    </>
   );
 };
