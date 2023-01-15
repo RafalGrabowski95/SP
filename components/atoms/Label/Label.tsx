0c2be7e3c4528ffce6ed0924eeb9a27220cbb0bd
Michał Pałys <michal.palys@jsql.pl>
2023-01-15 20:54:13 +0100

    label atom

--- components/atoms/Label/Label.tsx
+++ components/atoms/Label/Label.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Text } from 'components/atoms/Text';
+import { styled } from 'nativewind';
+import { Text as NativeText } from 'react-native';
+import classNames from 'classnames';
+import { View } from 'components/atoms/View';
 
+const StyledText = styled(NativeText);
 
-interface LabelProps {
-    text: string;
+interface IfLabelProps {
+    text?: string;
+    textSize?: 'xs' | 'sm' | 'base' | 'lg' | 'xl' | '2xl' | '3xl';
+    textWeight?: 'light' | 'normal' | 'medium' | 'semibold' | 'bold';
+    textAlign?: 'left' | 'center' | 'right' | 'justify' | 'start' | 'end';
+    textUppercase?: boolean;
 }
 
-export const Label = ({ text }: LabelProps) => {
+export const Label = ({ text='', textSize='base', textWeight='normal', textAlign='left', textUppercase=false }: IfLabelProps) => {
     return (
-        <Text>{text}</Text>
+        <View className="bg-surface-2 p-2 rounded-full">
+            <StyledText className={classNames(
+                'text-white pl-3 pr-3',
+                {
+                    [`text-${textSize}`]: true,
+                    [`text-${textWeight}`]: true,
+                    [`text-${textAlign}`]: true,
+                    'uppercase': textUppercase
+                }
+            )}>
+                {text}
+            </StyledText>
+        </View>
     );
 };
--- components/screens/Settings/Settings.tsx
+++ components/screens/Settings/Settings.tsx
@@ -1,5 +1,6 @@
 import { Text } from 'components/atoms/Text';
 import { Input } from 'components/atoms/Input';
+import { Label } from 'components/atoms/Label';
 
 export const Settings = () => {
   return (
@@ -8,6 +9,7 @@ export const Settings = () => {
         Settings
       </Text>
       <Input placeholder="elo" suffix="kg" />
+      <Label text='test' textSize='3xl' textAlign='center' textUppercase></Label>
     </>
   );
 };
